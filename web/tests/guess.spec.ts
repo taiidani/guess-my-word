7c7e5795e2cf42f590196e930f9f2ec9e7fc3bc0
Ryan Nixon <r.taiidani@gmail.com>
2022-12-27 07:54:16 -0800

    Slight cleanup

--- web/tests/guess.spec.ts
+++ web/tests/guess.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 const today = new Date(Date.UTC(2022, 11, 8, 0, 0, 0)).getTime()
 const tomorrow = new Date(Date.UTC(2022, 11, 9, 0, 0, 0)).getTime()
@@ -10,7 +10,7 @@ const hardYesterday = "nonjoiner"
 test.use({
     locale: 'en-US',
     timezoneId: 'America/Los_Angeles',
-});
+})
 
 test('guesses the default word', async ({ page }) => {
     await page.addInitScript(`{
@@ -28,50 +28,50 @@ test('guesses the default word', async ({ page }) => {
         const __DateNowOffset = ${today} - Date.now();
         const __DateNow = Date.now;
         Date.now = () => __DateNow() + __DateNowOffset;
-      }`);
+      }`)
 
-    await page.goto('/');
-    await expect(page).toHaveTitle(/Guess My Word/);
+    await page.goto('/')
+    await expect(page).toHaveTitle(/Guess My Word/)
 
-    await expect(page.locator('footer .col:nth-child(1)')).toContainText(defaultYesterday)
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('No guesses before the word')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+    const lWordEntry = page.getByPlaceholder('Enter a word here')
+    const lStatsYesterday = page.locator("#stats-yesterday")
+    const lGuessBefore = page.locator(".before")
+    const lGuessAfter = page.locator(".after")
+
+    await expect(lStatsYesterday).toContainText(defaultYesterday)
+    await expect(lGuessBefore).toHaveText('No guesses before the word')
+    await expect(lGuessAfter).toHaveText('No guesses after the word')
 
     // First guess
-    await page.locator('form#guesser input').type('medium')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+    await lWordEntry.type('medium')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('medium')
+    await expect(lGuessAfter).toHaveText('No guesses after the word')
 
     // Second guess
-    await page.locator('form#guesser input').type('tame')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('tame')
+    await lWordEntry.type('tame')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('medium')
+    await expect(lGuessAfter).toHaveText('tame')
 
     // Third guess
-    await page.locator('form#guesser input').type('right')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.before li:nth-child(2)')).toContainText('right')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('tame')
+    await lWordEntry.type('right')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('mediumright')
+    await expect(lGuessAfter).toHaveText('tame')
 
     // Fourth guess
-    await page.locator('form#guesser input').type('round')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.before li:nth-child(2)')).toContainText('right')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('round')
-    await expect(page.locator('.after li:nth-child(2)')).toContainText('tame')
+    await lWordEntry.type('round')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('mediumright')
+    await expect(lGuessAfter).toHaveText('roundtame')
 
     // Correct guess
-    await page.locator('form#guesser input').type(defaultToday)
-    await page.locator('form#guesser input').press('Enter')
+    await lWordEntry.type(defaultToday)
+    await lWordEntry.press('Enter')
     await expect(page.locator('#app')).toContainText('You guessed "' + defaultToday + '" correctly')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('round')
-    await expect(page.locator('.after li:nth-child(2)')).toContainText('tame')
-    await expect(page.locator('.after')).toContainText('round')
+    await expect(lGuessBefore).toHaveText('mediumright')
+    await expect(lGuessAfter).toHaveText('roundtame')
 })
 
 test('guesses the hard word', async ({ page }) => {
@@ -90,50 +90,50 @@ test('guesses the hard word', async ({ page }) => {
         const __DateNowOffset = ${today} - Date.now();
         const __DateNow = Date.now;
         Date.now = () => __DateNow() + __DateNowOffset;
-      }`);
+      }`)
 
-    await page.goto('/');
-    await expect(page).toHaveTitle(/Guess My Word/);
+    await page.goto('/')
+    await expect(page).toHaveTitle(/Guess My Word/)
+
+    const lWordEntry = page.getByPlaceholder('Enter a word here')
+    const lStatsYesterday = page.locator("#stats-yesterday")
+    const lGuessBefore = page.locator(".before")
+    const lGuessAfter = page.locator(".after")
 
     // Switch to hard
     await page.locator('#mode').selectOption('Hard')
-    await expect(page.locator('footer .col:nth-child(1)')).toContainText(hardYesterday)
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('No guesses before the word')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+    await expect(lStatsYesterday).toContainText(hardYesterday)
+    await expect(lGuessBefore).toHaveText('No guesses before the word')
+    await expect(lGuessAfter).toHaveText('No guesses after the word')
 
     // First guess
-    await page.locator('form#guesser input').type('medium')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+    await lWordEntry.fill('medium')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('medium')
+    await expect(lGuessAfter).toHaveText('No guesses after the word')
 
     // Second guess
-    await page.locator('form#guesser input').type('tame')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('tame')
+    await lWordEntry.fill('tame')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('medium')
+    await expect(lGuessAfter).toHaveText('tame')
 
     // Third guess
-    await page.locator('form#guesser input').type('oat')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.before li:nth-child(2)')).toContainText('oat')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('tame')
+    await lWordEntry.fill('oat')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('mediumoat')
+    await expect(lGuessAfter).toHaveText('tame')
 
     // Fourth guess
-    await page.locator('form#guesser input').type('out')
-    await page.locator('form#guesser input').press('Enter')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.before li:nth-child(2)')).toContainText('oat')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('out')
-    await expect(page.locator('.after li:nth-child(2)')).toContainText('tame')
+    await lWordEntry.fill('out')
+    await lWordEntry.press('Enter')
+    await expect(lGuessBefore).toHaveText('mediumoat')
+    await expect(lGuessAfter).toHaveText('outtame')
 
     // Correct guess
-    await page.locator('form#guesser input').type(hardToday)
-    await page.locator('form#guesser input').press('Enter')
+    await lWordEntry.type(hardToday)
+    await lWordEntry.press('Enter')
     await expect(page.locator('#app')).toContainText('You guessed "' + hardToday + '" correctly')
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('medium')
-    await expect(page.locator('.before li:nth-child(2)')).toContainText('oat')
-    await expect(page.locator('.after li:nth-child(1)')).toContainText('out')
-    await expect(page.locator('.after li:nth-child(2)')).toContainText('tame')
+    await expect(lGuessBefore).toHaveText('mediumoat')
+    await expect(lGuessAfter).toHaveText('outtame')
 })
--- web/tests/state.spec.ts
+++ web/tests/state.spec.ts
@@ -1,5 +1,4 @@
-import { test, expect } from '@playwright/test';
-import { padEnd } from 'core-js/core/string';
+import { test, expect } from '@playwright/test'
 
 const today = new Date(Date.UTC(2022, 11, 8, 0, 0, 0)).getTime()
 const tomorrow = new Date(Date.UTC(2022, 11, 9, 0, 0, 0)).getTime()
@@ -9,23 +8,23 @@ const hardToday = "oatcake"
 const hardYesterday = "nonjoiner"
 
 test.use({
-    locale: 'en-US',
-    timezoneId: 'America/Los_Angeles',
-});
+  locale: 'en-US',
+  timezoneId: 'America/Los_Angeles',
+})
 
 const sess = {
-    "version": 0.9,
-    "before": ["apple"],
-    "after": [],
-    "answer": "",
-    "guesses": 0,
-    "idleTime": 0,
-    "start": today,
-    "end": null,
+  "version": 0.9,
+  "before": ["apple"],
+  "after": [],
+  "answer": "",
+  "guesses": 0,
+  "idleTime": 0,
+  "start": today,
+  "end": null,
 }
 
 test('reads current state', async ({ page }) => {
-    await page.addInitScript(`{
+  await page.addInitScript(`{
         // Extend Date constructor to default to today
         Date = class extends Date {
           constructor(...args) {
@@ -40,22 +39,22 @@ test('reads current state', async ({ page }) => {
         const __DateNowOffset = ${today} - Date.now();
         const __DateNow = Date.now;
         Date.now = () => __DateNow() + __DateNowOffset;
-      }`);
+      }`)
 
-    await page.context().addInitScript(value => {
-        window.sessionStorage.setItem('state-default', JSON.stringify(value));
-    }, sess);
+  await page.context().addInitScript(value => {
+    window.sessionStorage.setItem('state-default', JSON.stringify(value))
+  }, sess)
 
-    await page.goto('/');
-    await expect(page).toHaveTitle(/Guess My Word/);
+  await page.goto('/')
+  await expect(page).toHaveTitle(/Guess My Word/)
 
-    await expect(page.locator('footer .col:nth-child(1)')).toContainText(defaultYesterday)
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('apple')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+  await expect(page.locator('#stats-yesterday')).toContainText(defaultYesterday)
+  await expect(page.locator('.before')).toHaveText('apple')
+  await expect(page.locator('.after')).toHaveText('No guesses after the word')
 })
 
 test('ignores prior state', async ({ page }) => {
-    await page.addInitScript(`{
+  await page.addInitScript(`{
         // Extend Date constructor to default to tomorrow
         Date = class extends Date {
           constructor(...args) {
@@ -70,16 +69,16 @@ test('ignores prior state', async ({ page }) => {
         const __DateNowOffset = ${tomorrow} - Date.now();
         const __DateNow = Date.now;
         Date.now = () => __DateNow() + __DateNowOffset;
-      }`);
+      }`)
 
-    await page.context().addInitScript(value => {
-        window.sessionStorage.setItem('state-default', JSON.stringify(value));
-    }, sess);
+  await page.context().addInitScript(value => {
+    window.sessionStorage.setItem('state-default', JSON.stringify(value))
+  }, sess)
 
-    await page.goto('/');
-    await expect(page).toHaveTitle(/Guess My Word/);
+  await page.goto('/')
+  await expect(page).toHaveTitle(/Guess My Word/)
 
-    await expect(page.locator('footer .col:nth-child(1)')).toContainText(defaultToday)
-    await expect(page.locator('.before li:nth-child(1)')).toContainText('No guesses before the word')
-    await expect(page.locator('.after')).toContainText('No guesses after the word')
+  await expect(page.locator('#stats-yesterday')).toContainText(defaultToday)
+  await expect(page.locator('.before')).toHaveText('No guesses before the word')
+  await expect(page.locator('.after')).toHaveText('No guesses after the word')
 })
